d348f6d8780f468659861367f21242cdf9dbaee2
Bumble <bumble.sakh@gmail.com>
2023-02-04 02:41:34 +1100

    feat: add artist endpoint

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,11 @@
 import { User } from 'src/user/entities/user.entity';
+import { Artist } from 'src/artist/entities/artist.entity';
 
 interface Store {
   users: User[];
+  artists: Artist[];
 }
 export const store: Store = {
   users: [],
+  artists: [],
 };
